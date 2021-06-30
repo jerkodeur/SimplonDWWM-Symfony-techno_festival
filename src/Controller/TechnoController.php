2bc4904693cad76b24af6d16a3397933fbaf23e8
jerkoLegion <jerome.potie@gmail.com>
2021-06-30 13:57:07 +0200

    Display the agenda page

--- src/Controller/TechnoController.php
+++ src/Controller/TechnoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ArtistRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,6 +14,19 @@ class TechnoController extends AbstractController
     {
         return $this->render('techno/home.html.twig', [
             'controller_name' => 'technoController',
+        ]);
+    }
+
+    #[Route('/agenda', name: 'agenda_home')]
+    public function agenda(ArtistRepository $artistRepository): Response
+    {
+        $days = ["20/08/2021", "21/08/2021", "22/08/2021"];
+        $hours= ["16h-18h", "18h-20h", "21h-23h"];
+
+        return $this->render('techno/agenda.html.twig', [
+            'days' => $days,
+            'hours' => $hours,
+            'artists' => $artistRepository->findByConcert()
         ]);
     }
 }
--- src/Repository/ArtistRepository.php
+++ src/Repository/ArtistRepository.php
@@ -19,22 +19,19 @@ class ArtistRepository extends ServiceEntityRepository
         parent::__construct($registry, Artist::class);
     }
 
-    // /**
-    //  * @return Artist[] Returns an array of Artist objects
-    //  */
-    /*
-    public function findByExampleField($value)
+    /**
+     * @return Artist[] Returns an array of Artist objects
+     */
+    public function findByConcert()
     {
         return $this->createQueryBuilder('a')
-            ->andWhere('a.exampleField = :val')
-            ->setParameter('val', $value)
+            ->select('a.concert', 'a.name')
+            ->where('a.concert IS NOT NULL')
             ->orderBy('a.id', 'ASC')
-            ->setMaxResults(10)
             ->getQuery()
             ->getResult()
         ;
     }
-    */
 
     /*
     public function findOneBySomeField($value): ?Artist
