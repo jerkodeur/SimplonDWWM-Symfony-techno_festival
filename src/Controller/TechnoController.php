5a4fcaa9fa63450bf9f3e42d8f9e7b064cbb691c
jerkoLegion <jerome.potie@gmail.com>
2021-06-30 16:31:19 +0200

    Hide the ticket page if not connected

--- src/Controller/TechnoController.php
+++ src/Controller/TechnoController.php
@@ -9,6 +9,16 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class TechnoController extends AbstractController
 {
+
+    private $days;
+    private $hours;
+
+    public function __construct()
+    {
+        $this->days = ["20/08/2021", "21/08/2021", "22/08/2021"];
+        $this->hours = ["16h-18h", "18h-20h", "21h-23h"];
+    }
+
     #[Route('/', name: 'techno_home')]
     public function index(): Response
     {
@@ -20,12 +30,10 @@ class TechnoController extends AbstractController
     #[Route('/agenda', name: 'agenda_home')]
     public function agenda(ArtistRepository $artistRepository): Response
     {
-        $days = ["20/08/2021", "21/08/2021", "22/08/2021"];
-        $hours= ["16h-18h", "18h-20h", "21h-23h"];
 
         return $this->render('techno/agenda.html.twig', [
-            'days' => $days,
-            'hours' => $hours,
+            'days' => $this->days,
+            'hours' => $this->hours,
             'artists' => $artistRepository->findByConcert()
         ]);
     }
@@ -33,12 +41,10 @@ class TechnoController extends AbstractController
     #[Route('/ticket/artist/{artist_id?0}', name: 'ticket_home')]
     public function ticket(ArtistRepository $artistRepository, int $artist_id): Response
     {
-        $days = ["20/08/2021", "21/08/2021", "22/08/2021"];
-        $hours= ["16h-18h", "18h-20h", "21h-23h"];
-
+        if(!$this->getUser()) return $this->redirectToRoute('artist_home');
         return $this->render('techno/ticket.html.twig', [
-            'days' => $days,
-            'hours' => $hours,
+            'days' => $this->days,
+            'hours' => $this->hours,
             'artists' => $artistRepository->findByConcert(),
             'artist' => $artistRepository->findOneBy(['id' => $artist_id])
         ]);
